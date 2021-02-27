1ab8c38882243da1ed80748816684e68c4041f7c
nsaleh <nsaleh@youxel.com>
2021-02-27 22:24:43 +0200

    add crud opertaions

--- src/app/modules/to-do/components/to-do/to-do.component.ts
+++ src/app/modules/to-do/components/to-do/to-do.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Todo } from '../../models/todo';
 
 @Component({
@@ -9,10 +9,28 @@ import { Todo } from '../../models/todo';
 export class ToDoComponent implements OnInit {
 
   @Input('todo') todo:Todo;
+  @Output('onEdit') onEdit = new EventEmitter<any>();
+  @Output('onDelete') onDelete = new EventEmitter<any>();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  /**
+   * `edit()` to emit todo item to edit it
+   * @param todo 
+   */
+  edit(todo){
+    this.onEdit.emit(todo);
+  }
+
+   /**
+   * `delete()` to emit todo item to delete it
+   * @param todo 
+   */
+  delete(todo){
+    this.onDelete.emit(todo);
   }
 
 }
--- src/app/modules/to-do/pages/to-do-list/to-do-list.component.ts
+++ src/app/modules/to-do/pages/to-do-list/to-do-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Todo } from '../../models/todo';
 
 @Component({
@@ -10,24 +11,121 @@ export class ToDoListComponent implements OnInit {
 
   todos: Todo[] = [
     {
-      id: '1',
+      id: 1,
       name: 'Task1',
       isDone: true
     },
     {
-      id: '2',
+      id: 2,
       name: 'Task2',
       isDone: false
     },
     {
-      id: '3',
+      id: 3,
       name: 'Task3',
       isDone: false
     }
-  ]
+  ];
+
+  todo: Todo;
+
+  isFormOpen: boolean;
+
+  isDeleteModalOpen: boolean;
+
+  todoForm: FormGroup;
+
+  isEditMode: boolean;
+
+
   constructor() { }
 
   ngOnInit(): void {
+    this.initTodoForm();
+
+  }
+
+  /**
+   * `initTodoForm()` to initialize form
+   */
+  initTodoForm() {
+    this.todoForm = new FormGroup({
+      name: new FormControl('', [Validators.required])
+    });
+  }
+  
+  /**
+   * `openEditForm()` to open form in edit mode
+   * @param todo 
+   */
+  openEditForm(todo: Todo) {
+    this.isFormOpen = true;
+    this.todo = todo;
+    this.todoForm.patchValue(todo);
+    this.isEditMode = true;
+  }
+
+  /**
+   * `openAddForm()` to opem form in add mode
+   */
+  openAddForm() {
+    this.initTodoForm();
+    this.isFormOpen = true;
+    this.isEditMode = false;
+  }
+
+  /**
+   * `openDeleteModal()` to open delete modal with this todo
+   * @param todo 
+   */
+  openDeleteModal(todo){
+    this.isDeleteModalOpen = true;
+    this.todo = todo;
+  }
+
+  /**
+   * `delete()` to delete todo from todo list
+   */
+  delete(){
+    let index = this.todos.findIndex(todo=>todo.id===this.todo.id);
+    this.todos.splice(index,1);
+    this.isDeleteModalOpen = false;
+  }
+
+  /**
+   * `save()` to save form
+   */
+  save() {
+    if (this.isEditMode) {
+      this.todos.map((todo, index) => {
+        if (todo.id === this.todo.id) {
+          todo.name = this.todoForm.get('name').value;
+        }
+      });
+    }
+    else {
+      this.todo = {
+        id: this.todos.length+1,
+        name: this.todoForm.get('name').value,
+        isDone: false
+      }
+      this.todos.push(this.todo);
+    }
+    this.isFormOpen = false;
+  }
+
+  /**
+   * `cancelForm()` to cancel form and close it
+   */
+  cancelForm() {
+    this.isFormOpen = false;
+  }
+
+  /**
+   * `cancelDelete()` to cancel delete and close it
+   */
+  cancelDelete() {
+    this.isDeleteModalOpen = false;
   }
 
 }
